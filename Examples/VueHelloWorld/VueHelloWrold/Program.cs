c51539de62ad95ea780feab299db338dcdc2cb13
Roman Vladimirov <trueromanus@gmail.com>
2025-04-27 20:26:45 +0300

    + Added example for Vue

---
+++ Examples/VueHelloWorld/VueHelloWrold/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using EmptyFlow.SciterAPI;
+
+SciterLoader.Initialize ( Environment.CurrentDirectory );
+var host = new SciterAPIHost ();
+host.LoadAPI ();
+host.CreateMainWindow ( 300, 300, enableDebug: true, enableFeature: true );
+host.LoadFile ( "home://hello-vue.htm" );
+host.Process ();
