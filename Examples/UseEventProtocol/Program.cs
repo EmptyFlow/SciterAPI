7e28cc80505ffaf661a8abb874ea92e7544b7739
Roman Vladimirov <trueromanus@gmail.com>
2025-12-10 22:03:57 +0300

    - Removed SOM example + Added Use File Protocol Example

---
+++ Examples/UseEventProtocol/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using EmptyFlow.SciterAPI;
+using System.Text;
+
+var host = new SciterAPIHost ( Environment.CurrentDirectory );
+host.CreateMainWindow ( 500, 500, enableDebug: true, enableFeature: true );
+host.Callbacks.AddProtocolHandler (
+  "http://", // we will be capture http protocol
+  (
+  path => {
+      if ( path != "http://data.json" ) return new byte[0]; // we handle only one address, others will be handled as usual
+
+      return Encoding.UTF8.GetBytes (
+""""
+{
+"items": [
+    {"id": 20},
+    {"id": 30}
+]
+}
+""""
+      );
+  }
+) );
+host.LoadFile ( "home://eventProtocol.htm" );
+host.Process ();
